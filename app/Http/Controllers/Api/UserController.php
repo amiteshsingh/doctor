4f05576388cc4e8d91c4178e74a7d26420fa03ca
krish202021 <prince.e3technology@gmail.com>
2026-06-17 10:23:47 +0530

    dr

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -22,8 +22,8 @@ class UserController extends Controller
     {
         return response()->json([
             'status'       => 200,
-            'version_code' => 13,
-            'version_name' => '2.1',
+            'version_code' => 14,
+            'version_name' => '2.3',
             'force_update' => false,
             'message'      => 'Naya update available hai!',
             'store_url'    => 'https://play.google.com/store/apps/details?id=com.rogisewa',
