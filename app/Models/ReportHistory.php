ae7ab942ccaf923818bdbbb743a8e459f2821739
krish202021 <prince.e3technology@gmail.com>
2026-08-19 14:08:07 +0530

    save h

--- app/Models/ReportHistory.php
+++ app/Models/ReportHistory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ReportHistory extends Model {
     protected $fillable = [
         'user_id', 'report_type', 'patient_info',
-        'summary', 'normal_count', 'abnormal_count', 'sections_json',
+        'summary', 'normal_count', 'abnormal_count', 'sections_json', 'image_path',
     ];
 
     public function user() {
